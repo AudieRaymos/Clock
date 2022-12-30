b3d0cb7a9aab4b94746ba9cfcb8a4ef0cc9bda37
Audie Raymos <audriannakraymos@gmail.com>
2022-12-30 08:17:43 -0800

    data selection

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function setClock() {
     const currentDate = new Date()
     const secondsRatio = currentDate.getSeconds() / 60
     const minuetsRatio = (secondsRatio + currentDate.getMinutes()) / 60
-    const hoursRatio = (minuetsRatio + currentDate.getHours()) / 60
+    const hoursRatio = (minuetsRatio + currentDate.getHours()) / 12
 }
